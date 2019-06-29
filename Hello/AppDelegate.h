6c90919b6728a2968d75af25cca14ceb7fa05b7d
yanggz <ygz012@163.com>
2019-06-30 06:54:14 +0800

    Initial Commit

---
+++ Hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by bluefish on 2019/6/30.
+//  Copyright © 2019 bluefish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello
+//
+//  Created by bluefish on 2019/6/30.
+//  Copyright © 2019 bluefish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
