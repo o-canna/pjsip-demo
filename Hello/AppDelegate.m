420898e0565d8c34cb356e9df937c23408bb2349
yanggz <ygz012@163.com>
2019-06-30 08:10:35 +0800

    test

--- Hello/AppDelegate.m
+++ Hello/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <pjsua.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [self testSIP];
     return YES;
 }
 
@@ -45,6 +47,32 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+-(void)testSIP {
+    
+    pjsua_create();
+    
+    pjsua_config ua_cfg;
+    pjsua_logging_config log_cfg;
+    pjsua_media_config media_cfg;
+    
+    pjsua_config_default(&ua_cfg);
+    pjsua_logging_config_default(&log_cfg);
+    pjsua_media_config_default(&media_cfg);
+    
+    pjsua_init(&ua_cfg, &log_cfg, &media_cfg);
+    
+    pjsua_transport_config transportConfig;
+    
+    pjsua_transport_config_default(&transportConfig);
+    
+    transportConfig.port = 5060;
+    
+    pjsua_transport_create(PJSIP_TRANSPORT_UDP, &transportConfig, NULL);
+    pjsua_transport_create(PJSIP_TRANSPORT_TCP, &transportConfig, NULL);
+    
+    pjsua_start();
 }
 
 
